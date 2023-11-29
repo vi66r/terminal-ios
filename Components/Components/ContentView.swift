c6b4eb3a9ba0ca2334dee27441be56409793aa72
Rafi Rizwan <rafi.rizwan.616@gmail.com>
2023-11-29 13:53:11 -0500

    hello Uploaded via Trace AI - https://trace.zip/c/331ea0e8-c6ca-43d2-999c-d5b3a0fc47cd

---
+++ Components/Components/ContentView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var inputText: String = ""
+    @State private var outputText: String = "Welcome to the Terminal.\n"
+    @FocusState private var isInputFieldFocused: Bool
+
+    var body: some View {
+        VStack(spacing: 0) {
+            ScrollView {
+                Text(outputText)
+                    .font(.system(size: UIFont.systemFontSize, weight: .regular, design: .monospaced))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .padding(.bottom, 20)
+                    .background(Color.black)
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+
+            HStack {
+                TextField("Enter command", text: $inputText)
+                    .focused($isInputFieldFocused)
+                    .font(.system(size: UIFont.systemFontSize, weight: .regular, design: .monospaced))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.clear)
+                    .cornerRadius(10)
+                    
+
+                Button(action: executeCommand) {
+                    Image(systemName: "arrow.right.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 22, height: 22)
+                        .foregroundColor(.white)
+                }
+            }
+            .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+            .padding(.bottom, isInputFieldFocused ? 320 : 0)
+        }
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.bottom)
+        .onChange(of: isInputFieldFocused) { focused in
+            if !focused {
+                hideKeyboard()
+            }
+        }
+    }
+    
+    func executeCommand() {
+        outputText += "$ \(inputText)\n"
+        inputText = ""
+        isInputFieldFocused = false
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
